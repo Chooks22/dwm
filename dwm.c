6b14583eaf8760d6b26ff98adbfd1fa8c6931337
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-03-26 23:16:00 +0800

    Switch from dmenu to rofi

--- dwm.c
+++ dwm.c
@@ -1960,8 +1960,6 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
