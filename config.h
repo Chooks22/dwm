923fd8685f1b0175e060ebbba5e750ccf4f6cd63
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-03-27 00:19:40 +0800

    Set layout configs

--- config.h
+++ config.h
@@ -1,23 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+
+/* gaps */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
+
+/* systray */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showsystray             = 1;   /* 0 means no systray */
+
+/* bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
 
+/* fonts */
+static const char *fonts[] = {
+	"JetBrains Mono:size=12",
+	"FontAwesome:size=12"
+};
+
+/* theme: Ancient One Dark - Violet */
 static const char bg[]          = "#24223a";
 static const char fg[]          = "#d5ced9";
 static const char bg_selected[] = "#2e2946";
